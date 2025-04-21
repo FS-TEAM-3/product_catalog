da0e3102b09abd53b6550d530082da2ef282cc58
PiekharievaK <pechareva@gmail.com>
2025-04-21 20:18:03 +0300

    routes mapping

--- src/pages/shared/Header/Header.tsx
+++ src/pages/shared/Header/Header.tsx
@@ -1,27 +1,33 @@
 import { NavLink } from 'react-router';
+import './style.module.scss';
 import s from './style.module.scss';
 
 export const Header = () => {
+  const getClassName = (isActive: boolean) => {
+    return isActive ? `${s.link} ${s.active}` : s.link;
+  };
+
+  const linksList = [
+    { name: 'Home', path: '/' },
+    { name: 'Cart', path: 'cart' },
+    { name: 'Catalog', path: 'catalog' },
+    { name: 'Favourites', path: 'favourites' },
+    { name: 'Item Card', path: 'phones' },
+    { name: 'Not Found', path: 'random' },
+  ];
+
   return (
     <>
-      <NavLink className={s.link} to="/">
-        Home
-      </NavLink>
-      <NavLink className={s.link} to="cart">
-        Cart
-      </NavLink>
-      <NavLink className={s.link} to="catalog">
-        Catalog
-      </NavLink>
-      <NavLink className={s.link} to="favourites">
-        Favourites
-      </NavLink>
-      <NavLink className={s.link} to="phones">
-        ItemCard
-      </NavLink>
-      <NavLink className={s.link} to="random">
-        Not Found
-      </NavLink>
+      {linksList.map(item => {
+        return (
+          <NavLink
+            className={({ isActive }) => getClassName(isActive)}
+            to={`${item.path}`}
+          >
+            {item.name}
+          </NavLink>
+        );
+      })}
     </>
   );
 };

ded7468da48ff4409318b5cbea01a95538cd6745
SofiiaPelenska <soniapelenskaoe2002@gmail.com>
2025-04-21 14:37:59 +0300

    add templates

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import '@radix-ui/themes/styles.css';
+import './styles/variables.scss';
+import { Home } from './pages/Home';
+import { Catalog } from './pages/Catalog';
+import { Favourites } from './pages/Favourites';
+import { Cart } from './pages/Cart';
+import { ItemCard } from './pages/ItemCard';
+
 function App() {
-  return <div>Product catalog</div>;
+  return (
+    <div>
+      <Home />
+      <Catalog />
+      <Favourites />
+      <Cart />
+      <ItemCard />
+    </div>
+  );
 }
 
 export default App;

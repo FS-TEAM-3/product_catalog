b15a4190fe6f8b0d2055f4d43c7fd1f36f979401
SofiiaPelenska <soniapelenskaoe2002@gmail.com>
2025-04-22 11:57:25 +0300

    add reactangle buttin and fonts

--- src/components/atoms/LinkButton/LinkButton.tsx
+++ src/components/atoms/LinkButton/LinkButton.tsx
@@ -1,6 +1,6 @@
 import { NavLink } from 'react-router-dom';
 import React from 'react';
-import styles from './_style.module.scss';
+import styles from './_styles.module.scss';
 
 type Props = {
   children: React.ReactNode;
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,3 +1,20 @@
+import { RectangleButton } from '@/components/atoms/RectangleButton';
+import styles from './_styles.module.scss';
+
 export const Home = () => {
-  return <div>Home page</div>;
+  return (
+    <>
+      <RectangleButton isActive={true}>Add to cart</RectangleButton>
+      <p></p>
+      <RectangleButton isLoading={true} size="big">
+        Added
+      </RectangleButton>
+      <div className={styles.container}>
+        <RectangleButton onClick={() => console.log('Added to cart')}>
+          Add to cart
+        </RectangleButton>
+      </div>
+      <div>Home page</div>
+    </>
+  );
 };

85600d13830e04c5866f8b2fa2659998711d926d
PiekharievaK <pechareva@gmail.com>
2025-04-22 17:05:45 +0300

    save before pull from main

--- src/pages/shared/Footer/Footer.tsx
+++ src/pages/shared/Footer/Footer.tsx
@@ -1,7 +1,36 @@
+import { Link } from 'react-router-dom';
+
+import s from './footer.module.scss';
+import { Container } from '@/components/atoms/Container';
+
 export const Footer = () => {
+  const linkList = [
+    { name: 'github', path: 'https://github.com/FS-TEAM-3' },
+    { name: 'contacts', path: 'https://github.com/FS-TEAM-3' },
+    { name: 'rights', path: '/rights' },
+  ];
+
   return (
-    <>
-      <h2>Footer</h2>
-    </>
+    <footer className={s.box}>
+      <Container>
+        <div>
+          <h2>Logo</h2>
+        </div>
+
+        <div>
+          <ul className={s.linkList}>
+            {linkList.map(item => (
+              <li>
+                <Link to={`${item.path}`} className={s.link}>
+                  {item.name.toUpperCase()}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div>Back to top Button</div>
+      </Container>
+    </footer>
   );
 };

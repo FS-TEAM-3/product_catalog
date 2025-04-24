bbc177ae3b0d95d48b5d3ea4ba67b24c837729bd
radykhovska <zagnibeda@i.ua>
2025-04-24 11:53:09 +0300

    feature/Description section(tech specs)

--- src/pages/ItemCard/ItemCard.tsx
+++ src/pages/ItemCard/ItemCard.tsx
@@ -1,8 +1,16 @@
+import { Description } from '@/components/molecules/Description';
+
+// it's temporary import
+import products from '../../../public/api/phones.json';
+///
+
 export const ItemCard = ({ category }: { category: string }) => {
+  const product = products[3];
   return (
     <div>
       <h4>{category}</h4>
       <p>ItemCard page</p>
+      <Description product={product} hasTitle={true} />
     </div>
   );
 };

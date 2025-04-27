8095ade142cea11fa60f206007ec52a7638bc42d
PiekharievaK <pechareva@gmail.com>
2025-04-27 09:01:31 +0300

    add evpty page icon

--- src/pages/Favourites/Favourites.tsx
+++ src/pages/Favourites/Favourites.tsx
@@ -4,7 +4,7 @@ import { ProductCard } from '@/components/organisms/ProductCard';
 import products from '../../../public/api/products.json';
 import s from '../Catalog/style.module.scss';
 import { Link } from 'react-router-dom';
-import { ChevronRight, House } from 'lucide-react';
+import { ChevronRight, HeartCrack, House } from 'lucide-react';
 
 export const Favourites = () => {
   const favouritesFromStore = useStore(store => store.favourites);
@@ -14,24 +14,36 @@ export const Favourites = () => {
 
   return (
     <Container className={s.catalog}>
-      <div className={s.catalog__breadcrumbs}>
-        <Link to={'/'} className={s.catalog__homeLink}>
-          <House className={s.catalog__homeIcon} />
-        </Link>
-        <ChevronRight className={s.catalog__chevronIcon} />
-        <span className={s.catalog__breadcrumbText}>Favourites</span>
-      </div>
-      <div className={s.catalog__header}>
-        <h2 className={s.catalog__title}>Favourites</h2>
-        <label className={s.catalog__label}>
-          {favouritesFromStore.length} items
-        </label>
-      </div>
-      <div className={s.catalog__grid}>
-        {favouritesToPage.map(item => (
-          <ProductCard path={`${item.category}/${item.name}`} product={item} />
-        ))}
-      </div>
+      {favouritesFromStore.length > 0 ? (
+        <>
+          <div className={s.catalog__breadcrumbs}>
+            <Link to={'/'} className={s.catalog__homeLink}>
+              <House className={s.catalog__homeIcon} />
+            </Link>
+            <ChevronRight className={s.catalog__chevronIcon} />
+            <span className={s.catalog__breadcrumbText}>Favourites</span>
+          </div>
+          <div className={s.catalog__header}>
+            <h2 className={s.catalog__title}>Favourites</h2>
+            <label className={s.catalog__label}>
+              {favouritesFromStore.length} items
+            </label>
+          </div>
+          <div className={s.catalog__grid}>
+            {favouritesToPage.map(item => (
+              <ProductCard
+                path={`${item.category}/${item.name}`}
+                product={item}
+              />
+            ))}
+          </div>
+        </>
+      ) : (
+        <Container>
+          <HeartCrack size={45} />
+          You haven't liked anything yet
+        </Container>
+      )}
     </Container>
   );
 };

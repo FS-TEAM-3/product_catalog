1a18249d59d12471b6b5e0efb26a0b6c7f91d34a
PiekharievaK <pechareva@gmail.com>
2025-04-27 08:48:55 +0300

    add favouritesPage from Catalog conponents & styles

--- src/pages/Favourites/Favourites.tsx
+++ src/pages/Favourites/Favourites.tsx
@@ -1,3 +1,37 @@
+import { useStore } from '../../store/store';
+import { Container } from '@/components/templates/Container';
+import { ProductCard } from '@/components/organisms/ProductCard';
+import products from '../../../public/api/products.json';
+import s from '../Catalog/style.module.scss';
+import { Link } from 'react-router-dom';
+import { ChevronRight, House } from 'lucide-react';
+
 export const Favourites = () => {
-  return <div>Favourites page</div>;
+  const favouritesFromStore = useStore(store => store.favourites);
+  const favouritesToPage = products.filter(product =>
+    favouritesFromStore.includes(product.name),
+  );
+
+  return (
+    <Container className={s.catalog}>
+      <div className={s.catalog__breadcrumbs}>
+        <Link to={'/'} className={s.catalog__homeLink}>
+          <House className={s.catalog__homeIcon} />
+        </Link>
+        <ChevronRight className={s.catalog__chevronIcon} />
+        <span className={s.catalog__breadcrumbText}>Favourites</span>
+      </div>
+      <div className={s.catalog__header}>
+        <h2 className={s.catalog__title}>Favourites</h2>
+        <label className={s.catalog__label}>
+          {favouritesFromStore.length} items
+        </label>
+      </div>
+      <div className={s.catalog__grid}>
+        {favouritesToPage.map(item => (
+          <ProductCard path={`${item.category}/${item.name}`} product={item} />
+        ))}
+      </div>
+    </Container>
+  );
 };

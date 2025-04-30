720c41ea2305c83d40c6b971cf0a39b8167ed4b4
Sofiia Pelenska <soniapelenskaoe2002@gmail.com>
2025-05-01 00:37:25 +0300

    saving chosen language to lacalstorage

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,22 +1,3 @@
-// import i18n from 'i18next';
-// import { initReactI18next } from 'react-i18next';
-// import en from './locales/en.json';
-// import uk from './locales/uk.json';
-
-// i18n.use(initReactI18next).init({
-//   resources: {
-//     en: { translation: en },
-//     uk: { translation: uk },
-//   },
-//   lng: 'en',
-//   fallbackLng: 'en',
-//   interpolation: {
-//     escapeValue: false,
-//   },
-// });
-
-// export default i18n;
-
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
@@ -24,7 +5,7 @@ import en from './locales/en.json';
 import uk from './locales/uk.json';
 
 i18n
-  .use(LanguageDetector) // додаємо детектор
+  .use(LanguageDetector)
   .use(initReactI18next)
   .init({
     resources: {
@@ -33,9 +14,7 @@ i18n
     },
     fallbackLng: 'en',
     detection: {
-      // спочатку дивимося в localStorage, потім в навігаторі
       order: ['localStorage', 'navigator'],
-      // кешуємо вибір у localStorage
       caches: ['localStorage'],
     },
     interpolation: { escapeValue: false },

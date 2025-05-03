247b3146c70c4989f74a3b12067bea77ed1a0e10
Sofiia Pelenska <soniapelenskaoe2002@gmail.com>
2025-05-03 22:05:56 +0300

    styling order page (not finished)

--- src/pages/Order/Order.tsx
+++ src/pages/Order/Order.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import operations from '@/utils/orderOperations';
 import { Container } from '@/components/templates/Container';
 import { GoBackButton } from '@/components/molecules/GoBackButton';
@@ -9,6 +9,7 @@ import { useStore } from '@/store/store';
 import { CartElement } from '@/types/Store';
 import { useTranslation } from 'react-i18next';
 import { RectangleButton } from '@/components/atoms/RectangleButton';
+import { useNavigate } from 'react-router-dom';
 
 type ModalData =
   | {
@@ -39,9 +40,15 @@ export const OrderPage = () => {
 
   const [modalData, setModalData] = useState<ModalData | null>(null);
   const [showModal, setShowModal] = useState(false);
-  const [showPopup, setShowPopup] = useState(true);
-
+  const navigate = useNavigate();
   const cart: CartElement[] = useStore(state => state.cart);
+
+  useEffect(() => {
+    document.body.style.overflow = showModal ? 'hidden' : '';
+    return () => {
+      document.body.style.overflow = '';
+    };
+  }, [showModal]);
 
   const onFormSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -74,103 +81,113 @@ export const OrderPage = () => {
     }));
   };
 
-  const closePopup = () => {
-    setShowPopup(false);
-  };
-
   return (
     <>
-      {showPopup && (
-        <div className={s.popupContainer}>
-          <div className={s.popup}>
-            <p>{t('order.info')}</p>
-            <button onClick={closePopup} className={s.closeButton}>
-              {t('order.close')}
+      {showModal && modalData ? (
+        <div className={s.modalOverlay}>
+          <div className={s.modal}>
+            {modalData.error ? (
+              <p>{modalData.message}</p>
+            ) : (
+              <>
+                <h3>Order Created</h3>
+                <p>
+                  <strong>Order ID:</strong> {modalData.orderId}
+                </p>
+                <p>
+                  <strong>Name:</strong> {modalData.owner.name}
+                </p>
+                <p>
+                  <strong>Email:</strong> {modalData.owner.email}
+                </p>
+                <p>
+                  <strong>Phone:</strong> {modalData.owner.phone}
+                </p>
+                <h4>Items:</h4>
+                <ul>
+                  {modalData.order.map(
+                    (item: { id: string; count: number }, index: number) => (
+                      <li key={index}>
+                        {item.id} — {item.count} pcs
+                      </li>
+                    ),
+                  )}
+                </ul>
+              </>
+            )}
+            <button
+              className={s.closeButton}
+              onClick={() => {
+                setShowModal(false);
+                navigate('/');
+              }}
+            >
+              HOME
             </button>
           </div>
         </div>
-      )}
-
-      <Container>
-        <div className={baseStyles.BackBtn}>
-          <GoBackButton />
-        </div>
-      </Container>
-      <Container>
-        <h1 className={`${s.title}`}>{t('order.title')}</h1>
-        <div className={s.auth}>
-          <form onSubmit={onFormSubmit} className={styles.authForm}>
-            <input
-              type="text"
-              name="name"
-              placeholder={t('auth.name')}
-              value={formData.name}
-              onChange={handleChange}
-            />
-            <input
-              type="text"
-              name="phone"
-              placeholder={t('order.phone')}
-              value={formData.phone}
-              onChange={handleChange}
-            />
-            <input
-              type="text"
-              name="email"
-              placeholder={t('auth.email')}
-              value={formData.email}
-              onChange={handleChange}
-            />
-            <input
-              type="text"
-              name="adress"
-              placeholder={t('order.adress')}
-              value={formData.adress}
-              onChange={handleChange}
-            />
-            <RectangleButton type="submit">{t('order.order')}</RectangleButton>
-          </form>
-        </div>
-      </Container>
-
-      {showModal && modalData && (
-        <div className={s.modal}>
-          {modalData.error ? (
-            <p>{modalData.message}</p>
-          ) : (
-            <>
-              <h3>Order Created</h3>
-              <p>
-                <strong>Order ID:</strong> {modalData.orderId}
-              </p>
-              <p>
-                <strong>Name:</strong> {modalData.owner.name}
-              </p>
-              <p>
-                <strong>Email:</strong> {modalData.owner.email}
-              </p>
-              <p>
-                <strong>Phone:</strong> {modalData.owner.phone}
-              </p>
-              <h4>Items:</h4>
-              <ul>
-                {modalData.order.map(
-                  (item: { id: string; count: number }, index: number) => (
-                    <li key={index}>
-                      {item.id} — {item.count} pcs
-                    </li>
-                  ),
-                )}
-              </ul>
-              <button
-                className={s.closeButton}
-                onClick={() => setShowModal(false)}
-              >
-                Close
-              </button>
-            </>
-          )}
-        </div>
+      ) : (
+        <>
+          <Container>
+            <div className={baseStyles.BackBtn}>
+              <GoBackButton />
+            </div>
+          </Container>
+          <Container>
+            <div>
+              <h1 className={`${s.title}`}>{t('order.title')}</h1>
+              <span className={`${s.titleMini}`}>{t('order.info')}</span>
+            </div>
+            <div className={s.auth}>
+              <form onSubmit={onFormSubmit} className={styles.authForm}>
+                <input
+                  type="text"
+                  name="name"
+                  placeholder={t('auth.name')}
+                  value={formData.name}
+                  onChange={handleChange}
+                  //required
+                />
+                <input
+                  type="phone"
+                  name="phone"
+                  placeholder={t('order.phone')}
+                  value={formData.phone}
+                  onChange={handleChange}
+                  //required
+                />
+                <input
+                  type="email"
+                  name="email"
+                  placeholder={t('auth.email')}
+                  value={formData.email}
+                  onChange={handleChange}
+                  //required
+                />
+                <input
+                  type="text"
+                  name="adress"
+                  placeholder={t('order.adress')}
+                  value={formData.adress}
+                  onChange={handleChange}
+                  //required
+                />
+                <RectangleButton type="submit">
+                  {t('order.order')}
+                </RectangleButton>
+              </form>
+              <div className={styles.authForm}>
+                {cart.map(item => {
+                  return (
+                    <span>
+                      {item.count} x {item.id}
+                    </span>
+                  );
+                })}
+              </div>
+            </div>
+          </Container>
+        </>
       )}
     </>
   );
